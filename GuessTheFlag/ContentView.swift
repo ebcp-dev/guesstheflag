3078b602d720735a64d7415bf175bea327c206da
Earl Perez <ebcp.dev@gmail.com>
2020-03-29 04:30:38 -0700

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Earl Perez on 3/29/20.
+//  Copyright © 2020 Earl Perez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
